585ce9d38c12a5f5b15701db192acfcaacfff1f4
Ravish <cto@sysotel.com>
2022-07-27 00:43:35 +0530

    update

--- src/Facades/IAM.php
+++ src/Facades/IAM.php
@@ -4,10 +4,12 @@ namespace SYSOTEL\APP\IAMConnector\Facades;
 
 use Illuminate\Support\Facades\Facade;
 use Psr\Http\Message\ResponseInterface;
+use SYSOTEL\APP\IAMConnector\Support\AdminDetails;
+use SYSOTEL\APP\IAMConnector\Support\AppUserDetails;
 
 /**
- * @method static ResponseInterface adminAuth(string|null $accessToken, null|string $permission = null): AdminDetails
- * @method static ResponseInterface appUserAuth(string|null $accessToken, int $propertyId = null, string $permission = null): AppUserDetails
+ * @method static AdminDetails adminAuth(string|null $accessToken, null|string $permission = null)
+ * @method static AppUserDetails appUserAuth(string|null $accessToken, int $propertyId = null, string $permission = null)
  *
  * @see IAM
  */
